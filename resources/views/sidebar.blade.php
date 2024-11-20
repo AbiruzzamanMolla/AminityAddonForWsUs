ca04565b738c97eb2b6d6d1588b6b755b7837613
Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2024-11-20 10:39:24 +0600

    restructuring folder

--- resources/views/sidebar.blade.php
+++
@@ -1,7 +0,0 @@
-@if (Module::isEnabled('Aminity') && Route::has('admin.listing.aminity.index'))
-    <li class="{{ Route::is('admin.listing.aminity.*') ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('admin.listing.aminity.index') }}">
-            <span>{{ __('Manage Aminity') }}</span>
-        </a>
-    </li>
-@endif
